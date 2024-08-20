1170c9ddcba9d337292bd67bd254c747812dfb12
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-08-20 14:55:09 -0400

    Added update employee endpoint to service layer

--- src/main/java/com/justpractice/demo/service/EmployeeService.java
+++ src/main/java/com/justpractice/demo/service/EmployeeService.java
@@ -1,11 +1,17 @@
 package com.justpractice.demo.service;
 
 import com.justpractice.demo.dto.EmployeeDto;
+import com.justpractice.demo.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
 
+    List<EmployeeDto> getAllEmployees();
 
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
 
 }
--- src/main/java/com/justpractice/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/justpractice/demo/service/EmployeeServiceImpl.java
@@ -6,13 +6,19 @@ import com.justpractice.demo.exception.EmployeeNotFoundException;
 import com.justpractice.demo.mapper.EmployeeMapper;
 import com.justpractice.demo.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+//    TODO: Refactor to Optionals and constructor injection
 
     private final EmployeeRepository employeeRepository;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
@@ -23,10 +29,32 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
+
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new EmployeeNotFoundException("Employee by id: " + employeeId + "does not exist."));
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::mapToEmployeeDto)   //lambda -> method references
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+
+       Employee employee =  employeeRepository.findById(employeeId).orElseThrow(
+                () -> new EmployeeNotFoundException("Employee by id: " + employeeId + "does not exist.")
+        );
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
 
 }
